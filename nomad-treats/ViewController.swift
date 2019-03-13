341c157d62346a97cf9950d9a04a09ea75934761
Daniel <messyjones@gmail.com>
2019-03-12 21:55:17 -0400

    first commit

---
+++ nomad-treats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nomad-treats
+//
+//  Created by Daniel Jones on 3/12/19.
+//  Copyright © 2019 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
